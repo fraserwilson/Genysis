5de86c8289b4afba599c112225d3924bf15e5431
Fraser Wilson <fraswilson@gmail.com>
2022-01-29 10:07:50 +0200

    initial model creation

---
+++ models/manager.dart
@@ -0,0 +1,11 @@
+class Manager{
+
+  final String name;
+  final String grade; // make into its own model
+  final String stats; // make into its own model
+  final String team; //use team model here
+
+  Manager(this.name,this.grade,this.stats,this.team);
+  
+
+}
---
+++ models/player.dart
@@ -0,0 +1,12 @@
+class Player{
+
+  final String name;
+  final String attack; //possibly change these into their own models for calulations
+  final String midfield;
+  final String defense;
+  final String keeping;
+
+  Player(this.name,this.attack,this.defense,this.midfield,this.keeping);
+  
+
+}
