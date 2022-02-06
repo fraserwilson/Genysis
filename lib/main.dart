5cb7bf03482a95069e5c3c2f57959842655adf02
Fraser Wilson <fraswilson@gmail.com>
2022-02-06 10:53:37 +0200

    starting login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:genysis/loginProcess/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-      ),
+      home:SingleChildScrollView(
+        child: Login(),
+      )
     );
   }
 }
