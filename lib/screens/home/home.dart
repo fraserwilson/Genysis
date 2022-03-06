6b78b14f5774b175928521a7cf34aaab49edcfa7
Fraser Wilson <fraswilson@gmail.com>
2022-03-06 17:10:15 +0200

    Accidentally added home page additions to this branch

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -21,6 +21,30 @@ class Home extends StatelessWidget {
           )
         ],
       ),
+      body: Container(
+        child: Center(child: Text("Chat app"),),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey.shade600,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: "Chats",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group_work),
+            label: "Channels",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box),
+            label: "Profile",
+          ),
+        ],
+      ),
     );
   }
 }
