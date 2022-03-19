7dddb557d4401c4e2436dec5873bf26c5697a76e
Fraser Wilson <fraswilson@gmail.com>
2022-03-19 11:53:19 +0200

    Got project working again with bottom nav bar switching screens

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:genysis/services/auth.dart';
 
-class Home extends StatelessWidget {
-  final AuthService _auth = AuthService();
 
+class Home extends StatefulWidget {
+  const Home({ Key? key }) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final AuthService _auth = AuthService();
+  final screens = [
+    Center(child: Text("Chats", style: TextStyle(fontSize: 60),),),
+    Center(child: Text("Team", style: TextStyle(fontSize: 60),),),
+    Center(child: Text("Player", style: TextStyle(fontSize: 60),),)
+  ];
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
@@ -21,11 +34,23 @@ class Home extends StatelessWidget {
           )
         ],
       ),
+      body: IndexedStack(
+        index: currentIndex,
+        children: screens,
+      ),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index){
+          setState(() {
+            currentIndex = index;
+          });
+        },
         selectedItemColor: Colors.blue,
         unselectedItemColor: Colors.grey.shade600,
         selectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
         unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+        selectedFontSize: 12,
+        showUnselectedLabels: false,
         type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(
